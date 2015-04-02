ff3ebfa25c9087a03933cbe2e7c020d65f22c782
sharingan069 <sgauravsharma059@gmail.com>
2015-04-02 13:37:04 -0400

    Initial Commit

---
+++ src/com/gaurav/client/JerseyClientGet.java
@@ -0,0 +1,36 @@
+package com.gaurav.client;
+ 
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+ 
+public class JerseyClientGet {
+ 
+	
+	  public static void main(String[] args) {
+		  
+			JerseyClientGet client = new JerseyClientGet();
+			client.getResponse();
+		}
+	 
+	 
+		private void getResponse() {
+			try {
+	 
+				Client client = Client.create();
+				WebResource webResource = client.resource("http://date.jsontest.com");
+				ClientResponse response = webResource.accept("application/xml").get(ClientResponse.class);
+				if (response.getStatus() != 200) {
+					throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+				}
+	 
+				String output = response.getEntity(String.class);
+				System.out.println("============getResponse============");
+				System.out.println(output);
+	 
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+}
